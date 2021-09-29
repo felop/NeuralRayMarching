657f2cc4c0bc22859c1290f7160668efee003588
Félox <lo.felox@gmail.com>
2021-09-29 12:47:41 +0200

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,11 +1,13 @@
+# loops over all pixels, calls rendererLIB to generate the pixel color from its coordinates, then saves the image
+
 import cv2
 import numpy as np
 import rendererLIB as lib
 from tqdm import tqdm
 from glob import glob
+
 images = glob("results/*.png")
 images = [int(images[im].split("\\")[1].split(".")[0]) for im in range(len(images))]
-
 picName = max(images) + 1
 
 coef = 1
@@ -19,9 +21,6 @@ for y in tqdm(range(len(img))):
         img[y][x] = lib.ImageGen(x,resolution[1]-y,resolution)  # calling lib to generate pixel color from pixel coordinates
 
 img *= 255
-#print(img[0])
-#print("TOTAL : "+str(np.sum(np.array(img))))
-
 img = cv2.resize(img,(800,450))
 
 cv2.imwrite("results/"+str(picName)+".png",img)
