1923427a8ee2421fe817a33aac9f1377d62f1f0a
Félix Lopez <contact@felixlop.com>
2021-09-29 10:20:12 +0200

    added ray marching renderer

---
+++ renderer.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import raymarcherLIB as lib
+from tqdm import tqdm
+from glob import glob
+images = glob("results/*.png")
+images = [int(images[im].split("\\")[1].split(".")[0]) for im in range(len(images))]
+
+picName = max(images) + 1
+
+coef = 1
+resolution = (80*coef,45*coef,3)
+img = np.zeros((resolution[1],
+                resolution[0],
+                resolution[2]))
+
+for y in tqdm(range(len(img))):
+    for x in range(len(img[y])):
+        img[y][x] = lib.ImageGen(x,resolution[1]-y,resolution)  # calling lib to generate pixel color from pixel coordinates
+
+img *= 255
+#print(img[0])
+#print("TOTAL : "+str(np.sum(np.array(img))))
+
+img = cv2.resize(img,(800,450))
+
+cv2.imwrite("results/"+str(picName)+".png",img)
